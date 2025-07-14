4d80af05abebae51a738ce61a8864ac422c12d56
Will Mobley <feulmob+github@gmail.com>
2025-07-14 10:47:02 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <BrowserRouter basename="/stream-gauge-distribution">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   );
 }
 
